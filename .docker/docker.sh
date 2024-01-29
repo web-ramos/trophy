4b78990643674e91b19795dd913f95df912e14c4
WebRamos <webmaster.ramos@gmail.com>
2024-01-29 21:51:03 +0100

    small doccker fixes

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# viperet - name & group current user (owner for logs directory and entity.json file)
-# export USER_ID=$(id -u viperet)
-# export GROUP_ID=$(id -g viperet)
-
-export USER_ID=$(id -u ramos)
-export GROUP_ID=$(id -g ramos)
+viperet - name & group current user (owner for logs directory and entity.json file)
+export USER_ID=$(id -u viperet)
+export GROUP_ID=$(id -g viperet)
 
 docker compose up -d
